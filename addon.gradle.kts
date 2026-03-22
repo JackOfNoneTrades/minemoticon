f4e08fcebb56f1de10b1d5f8ab9b4c0eb159c5a2
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-22 20:09:42 +0100

    blacklist mui on server dev runs

--- addon.gradle.kts
+++ addon.gradle.kts
@@ -5,7 +5,7 @@ tasks.withType<JavaExec>().configureEach {
         doFirst("wawelauthStripClientOnlyMods") {
             classpath = classpath.filter { file ->
                 val n = file.name
-                !n.contains("Baubles-Expanded", ignoreCase = true) && !n.contains("angelica", ignoreCase = true)
+                !n.contains("ModularUI2", ignoreCase = true) && !n.contains("Baubles-Expanded", ignoreCase = true) && !n.contains("angelica", ignoreCase = true)
             }
         }
     }
