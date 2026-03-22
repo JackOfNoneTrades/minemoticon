4607169200bd14e2fd65aaea622aa2cda0f53f80
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-22 19:49:26 +0100

    initial commit

---
+++ src/main/java/org/fentanylsolutions/minemoticon/core/EarlyMixinLoader.java
@@ -0,0 +1,24 @@
+package org.fentanylsolutions.minemoticon.core;
+
+import java.util.List;
+import java.util.Set;
+
+import org.fentanylsolutions.fentlib.core.FentEarlyMixinLoader;
+import org.fentanylsolutions.minemoticon.Minemoticon;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+@SuppressWarnings("unused")
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+public class EarlyMixinLoader extends FentEarlyMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins." + Minemoticon.MODID + ".early.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        return Mixins.getEarlyMixinsForLoader();
+    }
+}
